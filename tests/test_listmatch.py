be8d815173789ed0bf19a278a6b8579bf7b07874
Pokey Rule <pokey.rule@gmail.com>
2016-12-09 18:31:40 +0000

    Initial commit

---
+++ tests/test_listmatch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_listmatch
+----------------------------------
+
+Tests for `listmatch` module.
+"""
+
+
+import sys
+import unittest
+
+from listmatch import listmatch
+
+
+
+class TestListmatch(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
